4314d0e17df9905f141f9dd3abe98b7a91a9627c
pizzahuhthp9 <npwchr.work@gmail.com>
2021-04-27 02:17:43 +0700

    create brand model

---
+++ src/main/java/int221/project/project/models/Brand.java
@@ -0,0 +1,23 @@
+package int221.project.project.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "brands")
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String name;
+}
