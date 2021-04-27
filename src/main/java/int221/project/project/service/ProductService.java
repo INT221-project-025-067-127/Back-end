09d4759ffbde3206fd3a4d82510a3ec8d1158d14
pizzahuhthp9 <npwchr.work@gmail.com>
2021-04-27 15:05:51 +0700

    create product service

---
+++ src/main/java/int221/project/project/service/ProductService.java
@@ -0,0 +1,19 @@
+package int221.project.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import int221.project.project.models.Product;
+import int221.project.project.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+}
