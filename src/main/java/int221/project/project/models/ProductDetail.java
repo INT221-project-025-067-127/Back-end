580280be23a3f5189186bf255db40351cae45c0f
pizzahuhthp9 <npwchr.work@gmail.com>
2021-04-30 21:51:07 +0700

    remove unnecessary import

--- src/main/java/int221/project/project/models/ProductDetail.java
+++ src/main/java/int221/project/project/models/ProductDetail.java
@@ -1,13 +1,8 @@
 package int221.project.project.models;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
