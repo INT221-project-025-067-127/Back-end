473a57a555bc1f7381b62fb8f42f01dcf7b74145
pizzahuhthp9 <npwchr.work@gmail.com>
2021-05-02 15:44:35 +0700

    rework model

--- src/main/java/int221/project/project/models/ProductDetail.java
+++ src/main/java/int221/project/project/models/ProductDetail.java
@@ -3,8 +3,16 @@ package int221.project.project.models;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import int221.project.project.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +24,29 @@ import lombok.NoArgsConstructor;
 @Table(name = "quantity")
 public class ProductDetail {
     @EmbeddedId
+    @JsonIgnore
     private ProductDetailId productDetailId;
 
     @Column(name = "amount")
     private int amount;
 
+    @MapsId("productId")
+    @ManyToOne
+    @JoinColumn(name = "product_product_id")
+    private Product product;
+
+    @MapsId("colorId")
+    @ManyToOne
+    @JoinColumn(name = "color_color_id")
+    private Color color;
+
+    @MapsId("imageId")
+    @ManyToOne
+    @JoinColumn(name = "product_image_image_id")
+    private Image image;
+
+    @MapsId("sizeId")
+    @ManyToOne
+    @JoinColumn(name = "product_size_size_id")
+    private Size size;
 }
--- src/main/java/int221/project/project/models/ProductDetailId.java
+++ src/main/java/int221/project/project/models/ProductDetailId.java
@@ -2,6 +2,7 @@ package int221.project.project.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,17 +16,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Embeddable
 public class ProductDetailId implements Serializable {
-    @ManyToOne
-    private Color color;
+    // @ManyToOne
+    // private Color color;
 
-    @ManyToOne
-    private Product product;
+    // @ManyToOne
+    // private Product product;
 
-    @ManyToOne
-    @JoinColumn(name = "product_image_image_id")
-    private Image image;
+    // @ManyToOne
+    // @JoinColumn(name = "product_image_image_id")
+    // private Image image;
 
-    @ManyToOne
-    @JoinColumn(name = "product_size_size_id")
-    private Size size;
+    // @ManyToOne
+    // @JoinColumn(name = "product_size_size_id")
+    // private Size size;
+
+    @Column(name = "color_color_id")
+    private String colorId;
+    @Column(name = "product_product_id")
+    private String productId;
+    @Column(name = "product_image_image_id")
+    private String imageId;
+    @Column(name = "product_size_size_id")
+    private String sizeId;
+
 }
